c67f850a08596f12b635ac256c7a26f3b2e0fb42
Ethan Strominger <ethanstrominger2@gmail.com>
2022-08-13 02:23:00 -0400

    Added console.log and renamed js to js-backup

---
+++ build.md
@@ -0,0 +1,13 @@
+Questions:
+
+- production
+  - set env variables
+  - copy files to guide-pages directory - will this be a github directory?
+  - how to mark them to ignore them in github?
+- files that are in development will start wit
+  Requirements:
+- set environment variables that gives the process privileges to read and possibly write to Google Drive
+  Build process:
+- convert Google documents to markdown and copy them into guide-pages folder. By naming convention, exclude them from github by adding to .gitignore
+  - files that are in development, in review, or production will have their own folders
+- run jekyll
--- src/google-docs.js
+++ src/google-docs.js
@@ -1,61 +1,62 @@
-const {google} = require("googleapis")
-const GoogleOAuth2 = require("google-oauth2-env-vars")
+const { google } = require("googleapis");
+const GoogleOAuth2 = require("google-oauth2-env-vars");
 
-const {ENV_TOKEN_VAR} = require("./constants")
-const {GoogleDocument} = require("./google-document")
-const {writeDocumentToTests} = require("./write-document-to-tests")
-const {fetchFiles} = require("./google-drive")
+const { ENV_TOKEN_VAR } = require("./constants");
+const { GoogleDocument } = require("./google-document");
+const { writeDocumentToTests } = require("./write-document-to-tests");
+const { fetchFiles } = require("./google-drive");
 
 async function fetchDocument(id) {
   const googleOAuth2 = new GoogleOAuth2({
     token: ENV_TOKEN_VAR,
-  })
-  const auth = await googleOAuth2.getAuth()
+  });
+  const auth = await googleOAuth2.getAuth();
 
-  const res = await google.docs({version: "v1", auth}).documents.get({
+  const res = await google.docs({ version: "v1", auth }).documents.get({
     documentId: id,
-  })
+  });
 
   if (!res.data) {
-    throw new Error("Empty Data")
+    throw new Error("Empty Data");
   }
 
-  return res.data
+  return res.data;
 }
 
 /** @param {import('..').Options} options */
 async function fetchDocuments(options) {
-  const documentsProperties = await fetchFiles(options)
+  const documentsProperties = await fetchFiles(options);
   const links = documentsProperties.reduce(
-    (acc, properties) => ({...acc, [properties.id]: properties.slug}),
+    (acc, properties) => ({ ...acc, [properties.id]: properties.slug }),
     {}
-  )
+  );
+  console.log("exec promise");
 
   const googleDocuments = await Promise.all(
     documentsProperties.map(async (properties) => {
-      const document = await fetchDocument(properties.id)
+      const document = await fetchDocument(properties.id);
       const googleDocument = new GoogleDocument({
         document,
         properties,
         options,
         links,
-      })
+      });
 
       if (process.env.NODE_ENV === "DOCS_TO_TESTS") {
-        writeDocumentToTests(googleDocument)
+        writeDocumentToTests(googleDocument);
       }
 
-      return googleDocument
+      return googleDocument;
     })
-  )
+  );
 
   if (process.env.NODE_ENV === "DOCS_TO_TESTS") {
-    process.exit()
+    process.exit();
   }
 
-  return googleDocuments
+  return googleDocuments;
 }
 
 module.exports = {
   fetchDocuments,
-}
+};
