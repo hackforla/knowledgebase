ffd1a6c3d708ce768e58692205e66f2159622a65
Ethan Strominger <ethanstrominger2@gmail.com>
2022-11-03 12:12:28 -0400

    WIP: Simple Django config with one table

---
+++ DjangoRestApisPostgreSQL/knowledgebase/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+from knowledgebase.models import Tutorial
+from knowledgebase.serializers import TutorialSerializer
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def tutorial_list(request):
+    # GET list of tutorials, POST a new tutorial, DELETE all tutorials
+    print("tutorial_list")
+ 
+ 
+@api_view(['GET', 'PUT', 'DELETE'])
+def tutorial_detail(request, pk):
+    # find tutorial by pk (id)
+    try: 
+        tutorial = Tutorial.objects.get(pk=pk) 
+    except Tutorial.DoesNotExist: 
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+ 
+    # GET / PUT / DELETE tutorial
+    
+        
+@api_view(['GET'])
+def tutorial_list_published(request):
+    # GET all published tutorials
+    print("tutorial_list_published")
