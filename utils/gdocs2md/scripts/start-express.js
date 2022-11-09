585ca3c254740f3e706fa7536aba04b0031263be
Ethan Strominger <ethanstrominger2@gmail.com>
2022-11-09 11:27:57 -0500

    Move start-express to scripts, register model

---
+++ utils/gdocs2md/scripts/start-express.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { jekyllifyDocs } from "../src/jekyllUtils.js";
+import { config } from "dotenv";
+const envPath = path.resolve(process.cwd(), ".env.dev.local");
+config({ path: envPath }); // specifies the path to your .env file
+process.env.ENV_PATH = envPath;
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.get("/convert-all", (req, res) => {
+  const pluginOptions = {
+    // folder: folderId,
+    // targetMarkdownDir: root,
+    // imagesTarget: path.join(root, "assets/images"),
+    // suffix: suffix,
+    // matchPattern,
+    saveGdoc: false,
+    saveMarkdown: true,
+  };
+
+  jekyllifyDocs(pluginOptions);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
