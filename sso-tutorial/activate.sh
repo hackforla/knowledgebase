979b759707b47ae105ed8a71325e830a1c73f83d
Ethan Strominger <ethanstrominger2@gmail.com>
2023-06-13 09:37:27 -0400

    Lots of changes, still not working?

---
+++ sso-tutorial/activate.sh
@@ -0,0 +1,9 @@
+# return 0 will succeed if sourced, fail if not sourced
+# 2>/dev/null will suppress the error message
+(return 0 2>/dev/null) && sourced="true" || sourced="false"
+if [ "$sourced" = "true" ]; then
+    source venv/bin/activate
+    echo "DjangoRestApisPostgreSQL/start.sh: Sourced OK"
+else
+    echo "Error, script not sourced.  Please run 'source ./activate.sh'"
+fi
