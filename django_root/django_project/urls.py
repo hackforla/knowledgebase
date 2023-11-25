11068a41b69b7d99e8decfed540390c7c8bc2aa6
Ethan Strominger <ethanstrominger2@gmail.com>
2023-11-25 12:00:38 -0500

    Create url to get token

--- django_root/django_project/urls.py
+++ django_root/django_project/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from allauth.socialaccount.models import SocialAccount
+from django.shortcuts import render
 from django.urls import path, re_path, include
+
+def token_view(request):
+    account = SocialAccount.objects.get(user=request.user) 
+    token = account.socialtoken_set.all().order_by('-expires_at').first()
+
+    return render(request, "admin/token.html", { "token": token })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('token/', token_view),
     re_path(r'^', include('django_kb_app.urls')),
 ]
  
