0ea8ad37761d65b4130427f6a6c09877ad79e93b
Ethan Strominger <ethanstrominger2@gmail.com>
2024-01-07 07:56:41 -0500

    Complete autogenerate_serializer

--- django_root/kb/api/serializers.py
+++ django_root/kb/api/serializers.py
@@ -1,40 +1,28 @@
 from rest_framework import serializers 
 from kb.models import (
-    AssetType,
     TopicArea,
+    AssetType,
+    AssetType,
 )
 
 class TopicAreaSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = TopicArea
-        fields = (
-            "id",
-            "created_at",
-            "updated_at", 
-            "name",
-        )
-
-        read_only_fields = (
-            "id",
-            "created_at",
-            "updated_at,"
-        )
+        fields = ("id", "name", "created_at", "updated_at")
+        read_only_fields = ("id", "created_at", "updated_at")
         
+class AssetTypeSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = AssetType
+        fields = ("id", "created_at", "updated_at", "name")
+        read_only_fields = ("id", "updated_at", "created_at")
 
 class AssetTypeSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = AssetType
-        fields = (
-            "id",
-            "created_at",
-            "updated_at",
-            "name",
-        )
+        fields = ("id", "created_at", "updated_at", "name")
+        read_only_fields = ("id", "updated_at", "created_at")
 
-        read_only_fields = (
-            "id",
-            "updated_at",
-            "created_at",
-        )
--- django_root/kb/management/commands/autogenerate_serializer.py
+++ django_root/kb/management/commands/autogenerate_serializer.py
@@ -3,65 +3,61 @@ from django.apps import apps
 
 from django.core.management.base import BaseCommand
 class Command(BaseCommand):
-    help = "Populates data from json files or people depot"
+    help = "Adds code to serializers.py to serialize a model."
 
     def add_arguments(self, parser):
-         parser.add_argument('model_name', nargs='+', type=str)
+         parser.add_argument('app_name', type=str)
+         parser.add_argument('model_name', type=str)
 
     def handle(self, *__args__, **options):
-        model_name = options['model_name'][0]
-        generate(model_name)
+        app_name = options['app_name']
+        model_name = options['model_name']
+        generate(app_name, model_name)
 
-def generate(model_name):
+def generate(app_name, model_name):
+    print(f"Generating serializer for {app_name}.{model_name}", )
+    model = apps.get_model(app_name, model_name)
+
+    fields = [field.name for field in model._meta.get_fields() if not field.is_relation]
+    fields_text = ""
+    for index, field in enumerate(fields):
+        if index > 0:
+            fields_text += ', '
+        fields_text += f'"{field}"'
+
+    primary_key = model._meta.pk.name
+
     # Relative file path
-    relative_file_path = 'kb/api/serializers.py'
+    relative_file_path = f"{app_name}/api/serializers.py"
     relative_template_path = 'python_scripts/serializer_template.txt'
 
     # Complete file path
     file_path = os.path.join(os.getcwd(), relative_file_path)
     template_path = os.path.join(os.getcwd(), relative_template_path)
-
-    # Read existing content
-    with open(file_path, 'r') as file:
-        content = file.read()
-    
-    if content.find(model_name) != -1:
-        print("Serializer already exists")
-        return 1
-
-    model = apps.get_model("kb", model_name)
-
-
-    fields = [field.name for field in model._meta.get_fields() if not field.is_relation]
-    fields_text = ""
-    for field in fields:
-        fields_text += f'            "{field}",'
-        if field != fields[-1]:
-            fields_text += "\n"
-
-
-    id_text = ""
-    if ("uuid" in fields):
-        id_text += "uuid"
-    else:
-        id_text += "id"
-
     
     with open(template_path, 'r') as file:
         new_ending_text = file.read()   
     new_ending_text = new_ending_text.replace("{model_name}", model_name)
     new_ending_text = new_ending_text.replace("{fields_text}", fields_text) 
-    new_ending_text = new_ending_text.replace("{id_text}", id_text)
-    
-    lines = content.split("\n")
+    new_ending_text = new_ending_text.replace("{primary_key}", primary_key)
+
+    # Read existing content
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
     modified_content = ""
+    models_found = False
+    right_paren_found = False
     for line in lines:
-        modified_content += f"{line}\n"
-        if "from kb.models" in line:
+        if f"from {app_name}.models" in line and not models_found:
+            models_found = True
+        if ")" in line and models_found and not right_paren_found:
+            right_paren_found = True
             modified_content += f"    {model_name},\n"
+        modified_content += line
         
 
     with open(file_path, 'w') as file:
         file.write(modified_content + new_ending_text)
         
+       
