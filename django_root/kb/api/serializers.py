6a2b12274c90498805fba1b1ed2ebfd7b693a26c
Ethan Strominger <ethanstrominger2@gmail.com>
2024-01-07 09:52:00 -0500

    Move autogenerate scripts, refactor autogenerate_serializer

--- django_root/kb/api/serializers.py
+++ django_root/kb/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from kb.models import (
     TopicArea,
     AssetType,
-    AssetType,
 )
 
 class TopicAreaSerializer(serializers.ModelSerializer):
@@ -12,13 +11,7 @@ class TopicAreaSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "created_at", "updated_at")
         read_only_fields = ("id", "created_at", "updated_at")
         
-class AssetTypeSerializer(serializers.ModelSerializer):
-
-    class Meta: 
-        model = AssetType
-        fields = ("id", "created_at", "updated_at", "name")
-        read_only_fields = ("id", "updated_at", "created_at")
-
+        
 class AssetTypeSerializer(serializers.ModelSerializer):
 
     class Meta: 
--- django_root/kb/management/commands/autogenerate_serializer.py
+++ django_root/kb/management/commands/autogenerate_serializer.py
@@ -16,34 +16,27 @@ class Command(BaseCommand):
 
 def generate(app_name, model_name):
     print(f"Generating serializer for {app_name}.{model_name}", )
-    model = apps.get_model(app_name, model_name)
+ 
+    file_path = os.path.join(os.getcwd(), f"{app_name}/api/serializers.py")
+    content_with_import = add_import_to_text(file_path, app_name, model_name)
 
-    fields = [field.name for field in model._meta.get_fields() if not field.is_relation]
-    fields_text = ""
-    for index, field in enumerate(fields):
-        if index > 0:
-            fields_text += ', '
-        fields_text += f'"{field}"'
+    template_path = os.path.join(os.getcwd(), 'kb/management/commands/serializer_template.txt')
+    serializer_text = get_serializer_text(template_path, app_name, model_name)
+    if serializer_text == "":
+        print(f"Serializer for {app_name}.{model_name} already exists.")
+        return 1
 
-    primary_key = model._meta.pk.name
+    with open(file_path, 'w') as file:
+        file.write(content_with_import + serializer_text)
+        
+    return 0
 
-    # Relative file path
-    relative_file_path = f"{app_name}/api/serializers.py"
-    relative_template_path = 'python_scripts/serializer_template.txt'
-
-    # Complete file path
-    file_path = os.path.join(os.getcwd(), relative_file_path)
-    template_path = os.path.join(os.getcwd(), relative_template_path)
-    
-    with open(template_path, 'r') as file:
-        new_ending_text = file.read()   
-    new_ending_text = new_ending_text.replace("{model_name}", model_name)
-    new_ending_text = new_ending_text.replace("{fields_text}", fields_text) 
-    new_ending_text = new_ending_text.replace("{primary_key}", primary_key)
-
-    # Read existing content
+def add_import_to_text(file_path, app_name, model_name):
     with open(file_path, 'r') as file:
         lines = file.readlines()
+    already_exists = any(model_name in line for line in lines)
+    if already_exists:
+        return ""
 
     modified_content = ""
     models_found = False
@@ -55,9 +48,31 @@ def generate(app_name, model_name):
             right_paren_found = True
             modified_content += f"    {model_name},\n"
         modified_content += line
-        
+    return modified_content
 
-    with open(file_path, 'w') as file:
-        file.write(modified_content + new_ending_text)
+def get_serializer_text(template_path, app_name, model_name):
+    # set fields_text = list of fields in model
+    model = apps.get_model(app_name, model_name)
+    fields = [field.name for field in model._meta.get_fields() if not field.is_relation]
+    fields_text = ""
+    for index, field in enumerate(fields):
+        if index > 0:
+            fields_text += ', '
+        fields_text += f'"{field}"'
+
+    # set new_ending_text = text from serializer_template.txt
+    with open(template_path, 'r') as file:
+        new_text = file.read()   
+
+    # set primary_key = primary key of model
+    primary_key = model._meta.pk.name
+
+    # replace {model_name}, {fields_text}, and {primary_key} in new_ending_text
+    print("before", new_text)
+    new_text = new_text.replace("{model_name}", model_name)
+    new_text = new_text.replace("{fields_text}", fields_text) 
+    new_text = new_text.replace("{primary_key}", primary_key)
+    print("after", new_text)
+    return new_text
         
        
