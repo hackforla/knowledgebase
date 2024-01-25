6cfd884faf5575b8a82e5da99a2eed498fcc541a
We Care Insurance <toearkar.15@gmail.com>
2024-01-25 14:04:07 -0500

    Added Test Cases

--- django_root/kb/tests/test_get_apis.py
+++ django_root/kb/tests/test_get_apis.py
@@ -7,6 +7,7 @@ client = Client()
 pytestmark = pytest.mark.django_db
 ASSET_TYPE_URL = reverse("asset-type-list")
 TOPIC_AREA_URL = reverse("topic-area-list")
+PHASE_URL = reverse("phase-list")
 
 
 def test_asset_type(asset_type):
@@ -17,3 +18,8 @@ def test_asset_type(asset_type):
 def test_topic_area(topic_area):
     response = client.get(TOPIC_AREA_URL)
     assert response.json()[0]["name"] == topic_area.name
+
+
+def test_phase(phase):
+    response = client.get(PHASE_URL)
+    assert response.json()[0]["name"] == phase.name
