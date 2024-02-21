f62c5002672e6a23e8291afdab0caa33054eecfc
Ethan Strominger <ethanstrominger2@gmail.com>
2024-02-20 19:49:34 -0500

    Fix asset_type not null constraint in test

--- django_root/kb/tests/conftest.py
+++ django_root/kb/tests/conftest.py
@@ -11,13 +11,15 @@ from ..models import (
 # fmt: on
 
 asset_category_json = {"name": "Test name asset category"}
-asset_type_json = {"name": "Test name asset type"}
+asset_type_json = {"asset_category_id": 1, "name": "Test name asset type"}
 topic_area_json = {"name": "Test Topic Area"}
+
 
 @pytest.mark.django_db
 @pytest.fixture
 def asset_type():
     return AssetType.objects.create(**asset_type_json)
+
 
 @pytest.fixture
 def asset_category():
@@ -29,8 +31,8 @@ def topic_area():
     value = TopicArea.objects.create(name="Test Topic Area")
     return value
 
+
 @pytest.fixture
 def phase():
     value = Phase.objects.create(name="Test Phase")
     return value
-
